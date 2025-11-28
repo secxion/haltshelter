2e8465c8003fcfaf35ff90f770cdf9014878bc6a
secxion <secxionxii@gmail.com>
2025-11-27 18:29:36 -0700

    Add production frontend URL to whitelist2

--- server/app.js
+++ server/app.js
@@ -71,6 +71,8 @@ const whitelist = [
   'http://localhost:3001',
   'http://localhost:3002',
   'http://192.168.56.1:3001'
+  'https://haltshelter.onrender.com'
+  
 ];
 
 if (process.env.NODE_ENV === 'production' && process.env.FRONTEND_URL) {
